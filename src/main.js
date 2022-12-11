5f8353cdd59f4c7262d6dd720717db035a563b53
user <katalonij440@gmail.com>
2022-12-11 15:45:17 +0200

    add click button

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ getData("products/categories").then(createButtonList).catch(console.log);
 
 function createButtonList(items) {
   const buttonList = renderButtonItems(items);
-  refs.list.insertAdjacentHTML("beforeend", buttonList);
+  refs.listButton.insertAdjacentHTML("beforeend", buttonList);
 }
 
 getData("products").then(createCardList).catch(console.log);
@@ -16,4 +16,21 @@ getData("products").then(createCardList).catch(console.log);
 function createCardList(products) {
   const cardList = renderCards(products);
   refs.listCard.insertAdjacentHTML("beforeend", cardList);
+}
+
+refs.listButton.addEventListener("click", onClickButton);
+
+function onClickButton(event) {
+  if (event.target.nodeName != "BUTTON") {
+    return;
+  }
+  const button = event.currentTarget.querySelector(".is-active");
+  if (button) {
+    button.classList.remove("is-active");
+  }
+  const value = event.target.dataset.cat;
+  event.target.classList.add("is-active");
+
+  refs.listCard.innerHTML = "";
+  getData(`products/category/${value}`).then(createCardList).catch(console.log);
 }
